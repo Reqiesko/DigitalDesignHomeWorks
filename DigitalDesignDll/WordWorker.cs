1b60fd9874842c88078c3121b4af67c536f7b496
Артемий Нерадовский <reqieskorigin@gmail.com>
2023-05-22 22:17:09 +0300

    Добавьте файлы проекта.

---
+++ DigitalDesignDll/WordWorker.cs
@@ -0,0 +1,37 @@
+﻿namespace DigitalDesignDll
+{
+    public class WordWorker
+    {
+        private Dictionary<string, int> GetWordsCount(string inputFilePath)
+        {
+            // Создаём словарь для подсчёта уникальных слов
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            // Читаем входной файл и разбиваем его на слова
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string? line = reader.ReadLine();
+                    string[] words = line!.Split(new[] { ' ', '\t', ',', '.', ':', ';', '!', '?', '(', ')', '[', ']', '{', '}' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    // Добавляем слова в словарь или увеличиваем счётчик, если они уже есть в словаре
+                    foreach (string word in words)
+                    {
+                        string lowercaseWord = word.ToLower();
+                        if (!wordCounts.ContainsKey(lowercaseWord))
+                        {
+                            wordCounts[lowercaseWord] = 1;
+                        }
+                        else
+                        {
+                            wordCounts[lowercaseWord]++;
+                        }
+                    }
+                }
+            }
+
+            return wordCounts;
+        }
+    }
+}
---
+++ WordCount/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using DigitalDesignDll;
+
+namespace WordCount
+{
+    
+
+    class Program
+    {
+        static void Main()
+        {
+            var inputFilePath = Console.ReadLine();
+            var outputFilePath = Console.ReadLine();
+
+            var wordWorker = new WordWorker();
+
+            var methodInfo = typeof(WordWorker).GetMethod("GetWordsCount",
+                BindingFlags.Instance |
+                BindingFlags.Public |
+                BindingFlags.NonPublic);
+
+            var answerInvoke = methodInfo?.Invoke(wordWorker, parameters: new object?[] { inputFilePath });
+
+            var wordCounts = (Dictionary<string, int>)answerInvoke!;
+
+            // Сортируем словарь по убыванию количества употреблений слов
+            var sortedWordCounts = wordCounts.OrderByDescending(x => x.Value);
+
+            // Записываем результаты в выходной файл
+            using (var writer = new StreamWriter(outputFilePath!))
+            {
+                foreach (var wordCount in sortedWordCounts)
+                {
+                    writer.WriteLine("{0}\t{1}", wordCount.Key, wordCount.Value);
+                }
+            }
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
