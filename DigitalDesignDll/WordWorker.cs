cdf10e578c36df25939927ccdaef5727673ead35
Артемий Нерадовский <reqieskorigin@gmail.com>
2023-05-24 17:25:26 +0300

    Вот щас супер небольшой fix

--- DigitalDesignDll/WordWorker.cs
+++ DigitalDesignDll/WordWorker.cs
@@ -7,31 +7,6 @@
             // Создаём словарь для подсчёта уникальных слов
             Dictionary<string, int> wordCounts = new Dictionary<string, int>();
 
-            // Читаем входной файл и разбиваем его на слова
-            //using (StreamReader reader = new StreamReader(inputFilePath))
-            //{
-            //    while (!reader.EndOfStream)
-            //    {
-            //        string? line = reader.ReadLine();
-            //        string[] words = line!.Split(new[] { ' ', '\t', ',', '.', ':', ';', '!', '?', '(', ')', '[', ']', '{', '}' }, StringSplitOptions.RemoveEmptyEntries);
-            //
-            //        // Добавляем слова в словарь или увеличиваем счётчик, если они уже есть в словаре
-            //        foreach (string word in words)
-            //        {
-            //            string lowercaseWord = word.ToLower();
-            //            if (!wordCounts.ContainsKey(lowercaseWord))
-            //            {
-            //                wordCounts[lowercaseWord] = 1;
-            //            }
-            //            else
-            //            {
-            //                wordCounts[lowercaseWord]++;
-            //            }
-            //        }
-            //    }
-            //}
-
-
             string[] words = text.Split(new[] { ' ', '\t', ',', '.', ':', ';', '!', '?', '(', ')', '[', ']', '{', '}' }, StringSplitOptions.RemoveEmptyEntries);
 
             // Добавляем слова в словарь или увеличиваем счётчик, если они уже есть в словаре
